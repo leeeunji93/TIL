f349b13089af1d00b241bc6de9ebb667c71854cf
leeeunji93 <59554635+leeeunji93@users.noreply.github.com>
2020-03-02 17:46:36 +0900

    1st 컴포넌트 생성 및 이벤트 추가

---
+++ React/생코/생코실습/App.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            mode:"welcome",
+            selected_content_id:2,
+        subject:{title:"web",sub:"world!"},
+            welcome:{title:"welcome",desc:"Hello React!!"},
+        contents:[
+            {id:1,title:'HTML',desc:'HTML is...'},
+            {id:2,title:'CSS',desc:'CSS is...'},
+            {id:3,title:'JS',desc:'JS is...'}
+        ]
+        }
+    }
+    render() {
+        var _title, _desc = null;
+        if (this.state.mode === "welcome") {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        }else if (this.state.mode === "read") {
+            var i = 0;
+            while (i < this.state.contents.length) {
+                var data = this.state.contents[i];
+                if (data.id === this.state.selected_content_id) {
+                    _title = data.title;
+                    _desc = data.desc;
+                }
+            }
+        }
+
+
+        return (
+            <div className="App">
+               <Subject
+               title={this.state.subject.title}
+               sub={this.state.subject.sub}
+                   onChangePage={function () {
+                        this.setState({mode:"welcome"});
+               }.bind(this)}
+               >
+               </Subject>
+               <TOC
+               onChangePage={function(id){
+               this.setState({
+                   mode:"read",
+                   selected_content_id:Number(id)
+               });
+               }.bind(this)}
+               // 데이타라는 프롭스
+                   data={this.state.contents}
+                   ></TOC>
+                <Content title ={_title} desc = {_desc}></Content>
+            </div>
+        );
+    }
+}
+
+export default App;
