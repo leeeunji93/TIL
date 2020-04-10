e664843deb777c78a426ab2d1e87b66545acd6b6
leeeunji93 <59554635+leeeunji93@users.noreply.github.com>
2020-04-10 19:40:40 +0900

    Add files via upload

--- React/벨로퍼트 강의(전화번호부)/실습/App.js
+++ React/벨로퍼트 강의(전화번호부)/실습/App.js
@@ -1,90 +1,74 @@
 import React, {Component} from 'react';
-import PhoneForm from "./벨로퍼트/PhoneForm";
-import PhoneInfoList from "./벨로퍼트/PhoneInfoList";
+import PhoneForm from "./Component/PhoneForm";
+import PhoneInfoList from "./Component/PhoneInfoList";
 
 class App extends Component {
-    id = 3;
+    id= 1;
 
-    state={
-        information:[
-            {
-                id:0,
-                name:"홍길동",
-                phone:"010-7536-1526"
-            },
-            {
-                id:1,
-                name:"이은지",
-                phone:"011-7536-1526"
-            },
-            {
-                id:2,
-                name:"이창엽",
-                phone:"010-7536-1521"
-            }
-        ],
-        keyword:'',
-    }
+    state= {
+        information:[],
+        keyword:"",
+    };
 
-    handleChange = (e) => {
-        this.setState({
-            keyword:e.target.value,
 
+    handleChange = e => {
+       this.setState({
+            keyword: e.target.value
         })
     }
 
-    handleCreate = (data) =>{
-        const { information } = this.state;
+    handleCreate= data => {
+        const {information}= this.state;
         this.setState({
             information: information.concat({
                 ...data,
                 id:this.id++
+
+            })
+        })
+    };
+
+    handleRemove= id => {
+        const {information} = this.state;
+        this.setState({
+            information: information.filter(info => info.id !== id)
+        })
+    };
+
+    handleUpdate= (id,data) => {
+        const{information} = this.state;
+        this.setState({
+            information:information.map(info => {
+                if(info.id === id){
+                    return{
+                        ...data,
+                        id
+                    }
+                }
+                return id;
+
             })
         })
     }
 
-    handleReomve = (id) =>{
-        const { information } = this.state;
-        this.setState({
-            information:information.filter(info => info.id !== id)
-        })
-    }
 
-    handleUpdate = (id,data) => {
-        const { information } = this.state
-        this.setState({
-            information:information.map(
-                info =>{
-                    if(info.id === id){
-                        return{
-                            id,
-                            ...data
-                        };
-                    }
-                    return info;
-                }
-            )
-        })
-    }
     render() {
         return (
             <div>
-               <PhoneForm onCreate={this.handleCreate}/>
-               <input
-                   value={this.state.keyword}
-                   onChange={this.handleChange}
-                   placeholder="검색"
-               />
-               <PhoneInfoList
-                   data={this.state.information.filter(
-                       info =>
-                           info.name.indexOf(
-                               this.state.keyword
-                           ) > -1
-                   )}
-                   onRemove={this.handleRemove}
-                   onUpdate={this.handleUpdate}
-               />
+            <PhoneForm
+                onCreate={this.handleCreate}
+            />
+            <input
+                value={this.state.keyword}
+                onChange={this.handleChange}
+            />
+            <PhoneInfoList
+                information={this.state.information.filter(info => info.name.indexOf(this.state.keyword) > -1
+                )}
+                onRemove={this.handleRemove}
+                onUpdate={this.handleUpdate}
+
+            />
             </div>
         );
     }
